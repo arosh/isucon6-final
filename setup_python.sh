9922e22c5d6081b3e5b38ef5cd7ff5c13d672d6b
Ubuntu <isucon@isu01.wymxi5l1i3beznwgfjgyxqzw2b.lx.internal.cloudapp.net>
2017-11-24 13:33:52 +0000

    upgrade pip

--- setup_python.sh
+++ setup_python.sh
@@ -2,4 +2,5 @@
 set -ex
 sudo apt install -y python-virtualenv libmysqlclient-dev python-dev
 /usr/bin/virtualenv python/venv
+./python/venv/bin/pip install -U pip
 ./python/venv/bin/pip install -r python/requirements.freeze
